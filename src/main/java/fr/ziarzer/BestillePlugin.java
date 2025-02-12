65b01ed01b0a88a7d4af576babbe70993e755cd0
ZiarZer <dev@ziarzer.fr>
2025-02-12 14:14:20 +0100

    refactor: move update friendship levels task logic in the friendship manager

--- src/main/java/fr/ziarzer/BestillePlugin.java
+++ src/main/java/fr/ziarzer/BestillePlugin.java
@@ -4,31 +4,15 @@ import fr.ziarzer.commands.DbConnection;
 import fr.ziarzer.commands.FriendshipCommand;
 import fr.ziarzer.commands.SeeFriendshipAdminCommand;
 import fr.ziarzer.domain.FriendshipManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BestillePlugin extends JavaPlugin {
     private Connection dbConnection;
     private FriendshipManager friendshipManager;
-    public static final int MAX_DISTANCE = 100;
-    public static final int MAX_SQUARE_DISTANCE = MAX_DISTANCE * MAX_DISTANCE;
     public static final long TASK_TIMER_INTERVAL = 200L; // 10 seconds
-
-    private static double computeSquareDistance(Location a, Location b) {
-        return Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2) + Math.pow(a.getZ() - b.getZ(), 2);
-    }
-
-    private static Stream <? extends Player> getNearbyPlayers(Player player, Collection <? extends Player> onlinePlayers) {
-        return onlinePlayers.stream().filter(otherPlayer -> computeSquareDistance(player.getLocation(), otherPlayer.getLocation()) <= MAX_SQUARE_DISTANCE);
-    }
 
     @Override
     public void onEnable() {
@@ -40,12 +24,10 @@ public class BestillePlugin extends JavaPlugin {
             this.getCommand("seefriendship").setExecutor(new SeeFriendshipAdminCommand(friendshipManager));
 
             getLogger().info("Bestille plugin enabled");
-            getServer().getScheduler().runTaskTimer(this, () -> {
-                Collection<? extends Player> alivePlayers = Bukkit.getOnlinePlayers().stream().filter(player -> player.getHealth() >= 0).collect(Collectors.toList());
-                for (Player player : alivePlayers) {
-                    getNearbyPlayers(player, alivePlayers).filter(otherPlayer -> otherPlayer.getUniqueId().compareTo(player.getUniqueId()) > 0).forEach(otherPlayer -> friendshipManager.incrementFriendship(player, otherPlayer));
-                }
-            }, 0, TASK_TIMER_INTERVAL);
+
+            getServer()
+                    .getScheduler()
+                    .runTaskTimer(this, () -> friendshipManager.updateAlivePlayersFriendshipLevels(), 0, TASK_TIMER_INTERVAL);
         } catch (SQLException e) {
             getLogger().warning(e.getMessage());
         }
--- src/main/java/fr/ziarzer/domain/FriendshipManager.java
+++ src/main/java/fr/ziarzer/domain/FriendshipManager.java
@@ -2,20 +2,28 @@ package fr.ziarzer.domain;
 
 import fr.ziarzer.data.FriendshipRepository;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static fr.ziarzer.utils.StringUtils.colorText;
 
 public class FriendshipManager {
     public static final int MAX_FRIENDSHIP_LEVEL = 1200;
+    public static final int MAX_DISTANCE = 100;
+    public static final int MAX_SQUARE_DISTANCE = MAX_DISTANCE * MAX_DISTANCE;
     public FriendshipRepository repository;
     private final Logger logger;
 
     public FriendshipManager(Logger logger) throws SQLException {
         this.repository = new FriendshipRepository();
+        this.repository.createTable();
         this.logger = logger;
     }
 
@@ -50,5 +58,20 @@ public class FriendshipManager {
                 colorText(" are now ", ChatColor.LIGHT_PURPLE, false) +
                 colorText("best friends", ChatColor.LIGHT_PURPLE, true)
         );
+    }
+
+    private static double computeSquareDistance(Location a, Location b) {
+        return Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2) + Math.pow(a.getZ() - b.getZ(), 2);
+    }
+
+    private static Stream<? extends Player> getNearbyPlayers(Player player, Collection <? extends Player> onlinePlayers) {
+        return onlinePlayers.stream().filter(otherPlayer -> computeSquareDistance(player.getLocation(), otherPlayer.getLocation()) <= MAX_SQUARE_DISTANCE);
+    }
+
+    public void updateAlivePlayersFriendshipLevels() {
+        Collection<? extends Player> alivePlayers = Bukkit.getOnlinePlayers().stream().filter(player -> player.getHealth() >= 0).collect(Collectors.toList());
+        for (Player player : alivePlayers) {
+            getNearbyPlayers(player, alivePlayers).filter(otherPlayer -> otherPlayer.getUniqueId().compareTo(player.getUniqueId()) > 0).forEach(otherPlayer -> incrementFriendship(player, otherPlayer));
+        }
     }
 }
